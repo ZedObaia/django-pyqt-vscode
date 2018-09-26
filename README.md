04efbf00f8de1eb14d020fda51a1397be6efb371
Zeyad Obaia <zeyad.obaia@gmail.com>
2018-09-26 14:48:06 +0200

    tested on windows, add icon

--- README.md
+++ README.md
@@ -3,12 +3,18 @@
 VScode extension for <a href="https://github.com/ZedObaia/django-pyqt">django-pyqt</a>
 
 ## Features
+
 Control your django-pyqt project from within Vscode
 hit `Ctrl+Shift+P` and run one of the commands
+
 ## Extension Settings
+
+* First of all, run the configure command to set the python interpreter
+
 Read Instructions here
 <a href="https://github.com/ZedObaia/django-pyqt">django-pyqt</a> for information about how to configure these commands
 
+* `django-pyqt.init`: Configure the python interpreter
 * `django-pyqt.newProject`: Start new Project
 * `django-pyqt.uic`: Convert .ui files to .py files
 * `django-pyqt.rcc`: Convert .qrc files to .py files
@@ -18,13 +24,18 @@ Read Instructions here
 * `django-pyqt.deploy`: Deploy via pysinstaller
 * `django-pyqt.manage`: Run other manage.py commands
 
-
 ## Requirements
+
 * Python3
 * Django
 
 ## Release Notes
 
-### 0.0.1
+### 0.0.9
 
-Initial release of <a href="https://github.com/ZedObaia/django-pyqt">django-pyqt</a> for vscode
+Initial release of <a href="https://github.com/ZedObaia/django-pyqt">django-pyqt</a> for vscode tested on linux and windows.
+
+### TODO
+
+* Add animated gifs to demonstrate how the extension work
+* Test on mac.
--- extension.js
+++ extension.js
@@ -5,14 +5,62 @@ var fs = require("fs");
 var copydir = require("copy-dir");
 const { spawnSync } = require("child_process");
 
+
 let newprojdir;
-let template_path = path.join(
-  os.homedir(),
-  ".vscode",
-  "extensions",
-  "django-pyqt",
-  "template"
-);
+let jsonData;
+let python;
+try {
+  jsonData = require(path.join(__dirname, "data.json"));
+  python = jsonData.python_path;
+} catch (error) {
+  showError(error)
+}
+
+let template_path = path.join(__dirname, "template");
+
+function configure() {
+  showInfo("Please select python interpreter")
+  try 
+  {
+    showInfo("Current Interprter : "+python);
+  }
+  catch(error)
+  {
+    showError(error)
+  }
+  const OpenDialogOptions = {
+    defaultUri: new vscode.Uri({
+      scheme: "file",
+      path: "/usr/bin"
+    }),
+    openLabel: "Select Interpreter",
+    canSelectFiles: true,
+    canSelectFolders: false
+  };
+
+  if (os.platform() === "win32") {
+    OpenDialogOptions.defaultUri = new vscode.Uri({
+      scheme: "file",
+      path: path.join(os.homedir(),"Desktop")
+    });
+  }
+
+  vscode.window.showOpenDialog(OpenDialogOptions).then(uri => {
+    if (uri) {
+      python = uri[0].path;
+      if(os.platform() === "win32" && python.startsWith('/'))
+      {
+        python = python.replace('/', '')
+        python = python.replace(/[/]/g, "\\")
+      }
+      let conf = { python_path: python };
+      let data = JSON.stringify(conf);
+      fs.writeFileSync(path.join(__dirname, "data.json"), data);
+    } else {
+      showError("Interpreter was not slected");
+    }
+  });
+}
 
 function showInfo(word) {
   vscode.window.showInformationMessage(word);
@@ -21,29 +69,36 @@ function showError(word) {
   vscode.window.showErrorMessage(word);
 }
 
-function runManageCommand(command, dirName="", extension = "", dirOnly = false, general=false) {
+function runManageCommand(
+  command,
+  dirName = "",
+  extension = "",
+  dirOnly = false,
+  general = false
+) {
+  if(!python)
+    configure()
   const rootPath = vscode.workspace.rootPath;
   const usedDir = path.join(rootPath, dirName);
   const manage = path.join(rootPath, "manage.py");
-  if (general && fs.existsSync(manage))
-  {
-    if (command == "runserver")
-    {
-      showInfo("Whoohoo .. we don't do that here, this framework is serverless")
-      return
+  if (general && fs.existsSync(manage)) {
+    if (command == "runserver") {
+      showInfo(
+        "Whoohoo .. we don't do that here, this framework is serverless"
+      );
+      return;
     }
-    let out = spawnSync("python3", [manage, command]);
+    let out = spawnSync(python, [manage, command]);
     showInfo(out.stdout.toString());
     showError(out.stderr.toString());
-    return
+    return;
   }
-  
-  if (command === "deploy" && fs.existsSync(manage))
-  {
-    let out = spawnSync("python3", [manage, command]);
+
+  if (command === "deploy" && fs.existsSync(manage)) {
+    let out = spawnSync(python, [manage, command]);
     showInfo(out.stdout.toString());
     showError(out.stderr.toString());
-    return
+    return;
   }
   if (!fs.existsSync(usedDir) || !fs.existsSync(manage)) {
     showInfo("Please make sure you're inside the project directory");
@@ -63,11 +118,11 @@ function runManageCommand(command, dirName="", extension = "", dirOnly = false, 
   vscode.window.showQuickPick(all_files, quickPickOpts).then(selected => {
     if (selected) {
       if (selected === "All") {
-        let out = spawnSync("python3", [manage, command]);
+        let out = spawnSync(python, [manage, command]);
         showInfo(out.stdout.toString());
         showError(out.stderr.toString());
       } else {
-        let out = spawnSync("python3", [manage, command, selected]);
+        let out = spawnSync(python, [manage, command, selected]);
         showInfo(out.stdout.toString());
         showError(out.stderr.toString());
       }
@@ -81,9 +136,14 @@ function runManageCommand(command, dirName="", extension = "", dirOnly = false, 
 }
 
 function activate(context) {
+  let init_ = vscode.commands.registerCommand("extension.init", () =>
+    configure()
+  );
   let n_proj = vscode.commands.registerCommand(
     "extension.newProject",
     function() {
+      if(!python)
+      configure();
       const proj_name_input_opt = {
         placeHolder: "My Project",
         prompt: "Choose a cool name for your project",
@@ -93,14 +153,25 @@ function activate(context) {
       vscode.window.showInputBox(proj_name_input_opt).then(input => {
         if (input) {
           const OpenDialogOptions = {
-            defaultUri: vscode.Uri.file(os.homedir()),
+            defaultUri: vscode.Uri.file(path.join(os.homedir(), "Desktop")),
             openLabel: "Create here",
             canSelectFiles: false,
             canSelectFolders: true
           };
           vscode.window.showOpenDialog(OpenDialogOptions).then(uri => {
             if (uri) {
-              newprojdir = path.join(uri[0].path, input);
+              let temp = uri[0].path;
+              if(os.platform() === "win32")
+              {
+                if(temp.startsWith('/'))
+                {
+                  temp = temp.replace('/', '')
+                }
+
+                 temp = temp.replace(/[/]/g, "\\")
+
+              }
+              newprojdir = path.join(temp, input);
               const proj_uri = new vscode.Uri({
                 scheme: "file",
                 path: newprojdir
@@ -128,10 +199,11 @@ function activate(context) {
     }
   );
 
-
   let startapp_ = vscode.commands.registerCommand(
     "extension.startapp",
     function() {
+      if(!python)
+      configure()
       const rootPath = vscode.workspace.rootPath;
       const manage = path.join(rootPath, "manage.py");
       if (!fs.existsSync(manage)) {
@@ -152,7 +224,7 @@ function activate(context) {
           for (let i = 0; i < input_list.length; i++) {
             cmd_list.push(input_list[i]);
           }
-          const out = spawnSync("python3", cmd_list);
+          const out = spawnSync(python, cmd_list);
           showInfo(out.stdout.toString());
         } else {
           showInfo("Please enter valid app name");
@@ -160,7 +232,6 @@ function activate(context) {
       });
     }
   );
-
 
   let uic_ = vscode.commands.registerCommand("extension.uic", () =>
     runManageCommand("uic", "forms", ".ui", false)
@@ -170,7 +241,6 @@ function activate(context) {
     runManageCommand("rcc", "resources", ".qrc", false)
   );
 
-
   let makemigrations_ = vscode.commands.registerCommand(
     "extension.makemigrations",
     () => runManageCommand("makemigrations", "apps", "", true)
@@ -179,25 +249,28 @@ function activate(context) {
   let migrate_ = vscode.commands.registerCommand("extension.migrate", () =>
     runManageCommand("migrate", "apps", "", true)
   );
- 
+
   let deploy_ = vscode.commands.registerCommand("extension.deploy", () =>
-  runManageCommand("deploy")
-);
-  let manage_ = vscode.commands.registerCommand("extension.manage", () =>
-  {
-      const manageComOpts = {
+    runManageCommand("deploy")
+  );
+  let manage_ = vscode.commands.registerCommand("extension.manage", () => {
+    if(!python)
+    configure()
+    const manageComOpts = {
       placeHolder: "manage.py command",
       prompt: "Type a django manage.py command",
       password: false,
       ignoreFocusOut: true
     };
     vscode.window.showInputBox(manageComOpts).then(input => {
-    if (input) {
-      runManageCommand(input,"","",false,true)
-    } else {
-      showInfo("Please Enter a django command")
-    }})
+      if (input) {
+        runManageCommand(input, "", "", false, true);
+      } else {
+        showInfo("Please Enter a django command");
+      }
+    });
   });
+  context.subscriptions.push(init_);
   context.subscriptions.push(n_proj);
   context.subscriptions.push(uic_);
   context.subscriptions.push(rcc_);
@@ -206,7 +279,6 @@ function activate(context) {
   context.subscriptions.push(migrate_);
   context.subscriptions.push(deploy_);
   context.subscriptions.push(manage_);
-
 }
 exports.activate = activate;
 
